f523004d586db837fd8868e5413dfa4df3bedba2
Mark Kevin Besinga <markkevin.besinga@hcl.com>
2024-02-24 12:54:47 +0800

    install expo-linear-gradiant, added constant file

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, Dimensions } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { consonants, colorsArrays } from './constant';
 
 export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState(['#FF90BC', '#FFC0D9', '#F9F9E0', '#8ACDD7']); // Initial gradient colors
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ constant.js
@@ -0,0 +1,34 @@
+export const consonants = [
+  "Ba", "Be", "Bi", "Bo", "Bu",
+  "Da", "De", "Di", "Do", "Du",
+  "Ga", "Ge", "Gi", "Go", "Gu",
+  "Ha", "He", "Hi", "Ho", "Hu",
+  "Ka", "Ke", "Ki", "Ko", "Ku",
+  "La", "Le", "Li", "Lo", "Lu",
+  "Ma", "Me", "Mi", "Mo", "Mu",
+  "Na", "Ne", "Ni", "No", "Nu",
+  "Nga", "Nge", "Ngi", "Ngo", "Ngu",
+  "Pa", "Pe", "Pi", "Po", "Pu",
+  "Ra", "Re", "Ri", "Ro", "Ru",
+  "Sa", "Se", "Si", "So", "Su",
+  "Ta", "Te", "Ti", "To", "Tu",
+  "Wa", "We", "Wi", "Wo", "Wu",
+  "Ya", "Ye", "Yi", "Yo", "Yu"
+];
+
+export const colorsArrays = [
+  ['#FF90BC', '#FFC0D9', '#F9F9E0', '#8ACDD7'],
+  ['#C4DFDF', '#D2E9E9', '#E3F4F4', '#F8F6F4'],
+  ['#A1CCD1', '#F4F2DE', '#E9B384', '#7C9D96'],
+  ['#C2DEDC', '#ECE5C7', '#CDC2AE', '#116A7B'],
+  ['#FFB3B3', '#FFDBA4', '#FFE9AE', '#C1EFFF'],
+  ['#A7D2CB', '#F2D388', '#C98474', '#874C62'],
+  ['#FFBE98', '#FEECE2', '#F7DED0', '#E2BFB3'],
+  ['#B5C0D0', '#CCD3CA', '#F5E8DD', '#EED3D9'],
+  ['#8E7AB5', '#B784B7', '#E493B3', '#EEA5A6'],
+  ['#D7E4C0', '#C6DCBA', '#BBC3A4', '#B3A398'],
+  ['#B4B4B8', '#C7C8CC', '#E3E1D9', '#F2EFE5'],
+  ['#E1F0DA', '#D4E7C5', '#BFD8AF', '#99BC85'],
+  ['#F5EEE6', '#FFF8E3', '#F3D7CA', '#E6A4B4'],
+  ['#F9EFDB', '#EBD9B4', '#9DBC98', '#638889'],
+];
